7f4c9dad4865296195d9205772e9bfea899178dd
Anthony Ramírez López <arleyy1004@gmail.com>
2022-10-09 15:39:01 -0600

    changsdasdasdasdasdes to code

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
 <template>
   <div id="app" class="container">
     <div class="row" style="margin-top: 5%">
+      <router-link
+        class="button button-primary"
+        style="margin-right: 10px"
+        to="/artista"
+        >Artistas
+      </router-link>
     </div>
-    <router-view></router-view>
+    <router-view> </router-view>
   </div>
 </template>
 
