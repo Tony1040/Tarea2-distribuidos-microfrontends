093859b1ea673627cae5eefe9dc54364e4dac974
Anthony Ramírez López <arleyy1004@gmail.com>
2022-10-09 22:45:33 -0600

    cors artistas

--- netlify/functions/artistas.js
+++ netlify/functions/artistas.js
@@ -141,11 +141,10 @@ app.delete("/:id", (req, res) => {
 
 exp.use(bodyParser.json());
 exp.use("/.netlify/functions/artistas", app);
-app.use(function(req, res, next) {
+exp.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Headers', '*');
   next();
 });
 module.exports = exp;
