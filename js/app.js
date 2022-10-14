95e0720eb771c9fa8ce3c8fa89b28b8edd12a60d
Anthony Ramírez López <arleyy1004@gmail.com>
2022-10-13 22:28:17 -0600

    final changes to albums

--- js/app.js
+++ js/app.js
@@ -19,7 +19,14 @@ function getById(query) {
       var elem = data.find(checkId, query.params.id);
       var rendered = Mustache.render(template, elem);
       document.getElementById("content").innerHTML = rendered;
+
+      //Reference to artists 
       document.getElementById("id_artista").onclick = () => {
+        window.open("http://127.0.0.1:5173/get/?id=" + elem.id_artista)
+      }
+
+      //Reference to publishers
+      document.getElementById("id_discografica").onclick = () => {
         window.open("http://127.0.0.1:5175/get/?id=" + elem.id_artista)
       }
     });
